2d75aded55e42fca019f1eb585b09750dda85336
Andy <andy.su7117@gmail.com>
2023-08-22 21:20:23 +1000

    user, destination. comment models

---
+++ Project 2/models/Destination.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Destination extends Model {}
+
+Destination.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'destination',
+  }
+);
+
+module.exports = Destination;
