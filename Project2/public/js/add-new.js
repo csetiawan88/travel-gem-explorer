f585b5cbffcd8ddfc416c52705fc3f674fb86303
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-08-23 23:33:22 +1000

    added add new destination button

---
+++ Project2/public/js/add-new.js
@@ -0,0 +1,4 @@
+    document.getElementById('showAddDestination').addEventListener('click', function() {
+        const container = document.getElementById('addDestinationContainer');
+        container.style.display = container.style.display === 'none' ? 'block' : 'none';
+    });
