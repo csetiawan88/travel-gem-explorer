4514adfde421894494a38640c98a1189e6c5b739
Andy <andy.su7117@gmail.com>
2023-08-23 20:30:53 +1000

    model index.js

--- Project 2/models/index.js
+++ Project 2/models/index.js
@@ -6,3 +6,20 @@ User.hasMany(Destination, {
   foreignKey: 'userId',
   onDelete: 'Cascade',
 });
+
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'Cascade',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+Destination.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+Comment.hasOne(Destination, {
+  foreignKey: 'destinationId',
+});
