2a9ea3c535c6f49e5483ff2adc3a7eca743943ab
Youngobz <133522178+Youngobz@users.noreply.github.com>
2023-08-25 01:21:30 +1000

    controllers updated

--- controllers/DestinationController.js
+++ controllers/DestinationController.js
@@ -1,16 +1,27 @@
-const { Destination } = require('../models');
+const { Destination } = require('../../models');
 
-const DestinationController = {
-  getAllDestinations: async (req, res) => {
-    try {
-      const destinations = await Destination.findAll();
-      res.render('destination-list', { destinations }); 
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
-    }
-  },
-
+exports.getDestination = (req, res) => {
+    Destination.findAll()
+    .then(destinyData => res.json(destinyData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
 };
 
-module.exports = DestinationController;
+exports.getDestinationById = (req, res) => {
+    Destination.findOne({
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then (destinyData => {
+      if (! destinyData) {
+        res.status(404).json({ message: 'No Category found with this id'});
+        return;
+      }
+      res.json(destinyData);
+    })
+    .catch (err => {
+      res.status(500).json(err);
+    });
+};
