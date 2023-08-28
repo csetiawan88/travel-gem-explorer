91bb4c07dbd722389460adf94eaa9bc8afed0715
Andy <andy.su7117@gmail.com>
2023-08-28 20:38:01 +1000

    fixed seeds errors

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,30 +1,29 @@
 // Comments
-const { destinationComment } = require('../models');
-
+const { Comment } = require('../models');
 
 const commentData = [
   {
     userId: 1,
     destinationId: 1,
-    comment: "Fantastic!"
+    comment: 'Fantastic!',
   },
   {
     userId: 2,
     destinationId: 2,
-    comment: "This is great news!"
+    comment: 'This is great news!',
   },
   {
     userId: 3,
     destinationId: 3,
-    comment: "Amazing work!"
+    comment: 'Amazing work!',
   },
   {
     userId: 4,
     destinationId: 4,
-    comment: "Keep up the good work!"
+    comment: 'Keep up the good work!',
   },
-]
+];
 
-const seedComments = () => destinationComment.bulkCreate(commentData);
+const seedComments = () => Comment.bulkCreate(commentData);
 
 module.exports = seedComments;
