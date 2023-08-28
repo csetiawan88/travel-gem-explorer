353ce79aef2b4a0667ff5aad452516b2ac1a3f88
Youngobz <133522178+Youngobz@users.noreply.github.com>
2023-08-28 18:44:43 +1000

    destination controller updated

--- controllers/DestinationController.js
+++ controllers/DestinationController.js
@@ -15,7 +15,7 @@ exports.getDestinationById = (req, res) => {
       },
     })
     .then (destinyData => {
-      if (! destinyData) {
+      if (!destinyData) {
         res.status(404).json({ message: 'No Destination found with this id'});
         return;
       }
@@ -42,4 +42,42 @@ exports.getDestinationByUserId = (req, res) => {
   .catch (err => {
     res.status(500).json(err);
   });
+};
+
+exports.createDestination = (req, res) => {
+  Destination.create({
+    name: req.body.name,
+    image_source: req.body.image_url,
+    description: req.body.description,
+    userId: req.body.userId,
+  })
+  .then (destinyData => res.json(destinyData))
+  .catch (err => {
+    res.status(500).json(err);
+  });
+}
+
+exports.updateDestination = (req, res) => {
+  Destination.update({
+    name: req.body.name,
+    image_source: req.body.image_url,
+    description: req.body.description,
+    userId: req.body.userId,
+  },
+  {
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then (destinyData => {
+    if (!destinyData) {
+      res.status(404).json({massage: 'No Category found with this id'});
+      return;
+    }
+    res.json(destinyData);
+  })
+  .catch (err => {
+      // console.log(err);
+    res.status(500).json(err);
+  }); 
 };
