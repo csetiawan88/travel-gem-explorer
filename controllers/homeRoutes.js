f26c49f6ab35a250529c92f8b787b35a91c18a12
Andy <andy.su7117@gmail.com>
2023-08-29 16:57:49 +1000

    rendered the homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Destination, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const destinationData = await Destination.findAll();
+
+    const destinations = destinationData.map((project) =>
+      project.get({ plain: true })
+    );
+
+    res.render('homepage', { destinations, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/destination/:id', async (req, res) => {
+  try {
+    const destinationData = await Destination.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: ['comment', 'userId'],
+        },
+      ],
+    });
+
+    const destination = destinationData.get({ plain: true });
+    res.render('destinations', {
+      ...destination,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -38,63 +38,64 @@ app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 // Handling the login form submission
-app.post('/login', (req, res) => {
-  // Handle login logic here
-});
+// app.post('/login', (req, res) => {
+//   // Handle login logic here
+// });
 
-// Handling the signup form submission
-app.post('/signup', (req, res) => {
-  // Handle signup logic here
-});
+// // Handling the signup form submission
+// app.post('/signup', (req, res) => {
+//   // Handle signup logic here
+// });
 
-app.get('/', (req, res) => {
-  const pageTitle = 'Home';
-  let modalTitle = 'Login or Sign Up'; // Default modal title
+// app.get('/', (req, res) => {
+//   const pageTitle = 'Home';
+//   let modalTitle = 'Login or Sign Up'; // Default modal title
 
-  // Check if the request contains an action parameter (login or signup)
-  if (req.query.action) {
-    if (req.query.action === 'login') {
-      modalTitle = 'Login';
-    } else if (req.query.action === 'signup') {
-      modalTitle = 'Sign Up';
-    }
-  }
+//   // Check if the request contains an action parameter (login or signup)
+//   if (req.query.action) {
+//     if (req.query.action === 'login') {
+//       modalTitle = 'Login';
+//     } else if (req.query.action === 'signup') {
+//       modalTitle = 'Sign Up';
+//     }
+//   }
 
-  res.render('homepage', { pageTitle, modalTitle });
-});
+//   res.render('homepage', { pageTitle, modalTitle });
+// });
 
-app.get('/add-new', (req, res) => {
-  const pageTitle = 'Add New Destination';
-  //   const loggedIn = req.isAuthenticated();
+// app.get('/add-new', (req, res) => {
+//   const pageTitle = 'Add New Destination';
+//   //   const loggedIn = req.isAuthenticated();
 
-  res.render('add-new', {
-    pageTitle,
-    // , loggedIn
-  });
-});
+//   res.render('add-new', {
+//     pageTitle,
+//     // , loggedIn
+//   });
+// });
 
-app.get('/destination/:id', async (req, res) => {
-  try {
-    // Fetch destination data using the :id parameter
-    const destinationId = req.params.id;
-    // Fetch destination data from the database using the ID
+// app.get('/destination/:id', async (req, res) => {
+//   try {
+//     // Fetch destination data using the :id parameter
+//     const destinationId = req.params.id;
+//     // Fetch destination data from the database using the ID
 
-    // For now, let's assume destinationData contains the necessary data
-    const destinationData = {
-      name: 'Destination Name',
-      location: 'Destination Location',
-      description: 'Destination Description',
-      // ... other data
-    };
+//     // For now, let's assume destinationData contains the necessary data
+//     const destinationData = {
+//       name: 'Destination Name',
+//       location: 'Destination Location',
+//       description: 'Destination Description',
+//       // ... other data
+//     };
 
-    res.render('destination', destinationData);
-  } catch (error) {
-    console.error('Error fetching destination data:', error);
-    res.status(500).send('An error occurred while fetching destination data.');
-  }
-});
+//     res.render('destination', destinationData);
+//   } catch (error) {
+//     console.error('Error fetching destination data:', error);
+//     res.status(500).send('An error occurred while fetching destination data.');
+//   }
+// });
 
 app.use(routes);
+
 // Start the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
