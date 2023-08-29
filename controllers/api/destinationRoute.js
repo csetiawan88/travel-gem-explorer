da60a619c7d85702bb26c5264113c610d6a89ffc
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-08-29 19:31:44 +1000

    added routers

---
+++ controllers/api/destinationRoute.js
@@ -0,0 +1,98 @@
+const router = require('express').Router();
+const { Destination } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const destinations = await Destination.findAll();
+
+    res.status(200).json(destinations);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const destinationId = req.params.id;
+  try {
+    const destination = await Destination.findByPk(destinationId);
+
+    if (!destination) {
+      res.status(404).json({ message: 'Destination not found' });
+      return;
+    }
+
+    res.status(200).json(destination);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/userDestination/:userId', async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const destinations = await Destination.findAll({
+      where: {
+        userId: userId,
+      },
+    });
+
+    res.status(200).json(destinations);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/create-destination', withAuth, async (req, res) => {
+  try {
+    const newDestination = await Destination.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+
+    res.status(200).json(newDestination);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const destinationId = req.params.id;
+    const destinationToUpdate = await Destination.findByPk(destinationId);
+
+    if (!destinationToUpdate) {
+      res.status(404).json({ message: 'Destination not found' });
+      return;
+    } 
+    
+    await destinationToUpdate.update(req.body);
+
+    res.status(200).json(destinationToUpdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const destinationData = await Destination.destroy({
+      where: {
+        id: req.params.id,
+        userId: req.session.userId,
+      },
+    });
+
+    if (!destinationData) {
+      res.status(404).json({ message: 'No destination found with this id!' });
+      return;
+    }
+
+    res.status(200).json(destinationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
