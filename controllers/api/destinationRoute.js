6b4cb16a107fa295d923587a162804df5e3ba4d7
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-08-30 09:15:54 +1000

    log out button styling

--- controllers/api/destinationRoute.js
+++ controllers/api/destinationRoute.js
@@ -2,16 +2,6 @@ const router = require('express').Router();
 const { Destination, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    const destinations = await Destination.findAll();
-
-    res.status(200).json(destinations);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get('/userDestination/:userId', async (req, res) => {
   const userId = req.params.userId;
 
@@ -68,25 +58,35 @@ router.delete('/:id', withAuth, async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    console.log('Request Params:', req.params);
-    const destinationData = await Destination.findByPk(req.params.id, {
-      include: [
-        {
-          model: Comment,
-          attributes: ['comment', 'userId'],
-        },
-      ],
+    const destinationId = req.params.id;
+
+    // Fetch destination data from the database based on the ID
+    const destinationData = await Destination.findByPk(destinationId);
+
+    if (!destinationData) {
+      return res.status(404).send('Destination not found');
+    }
+
+    const latestDestinations = await Destination.findAll({
+      attributes: ['id', 'image_source', 'name'],
+      limit: 3,
+      order: [['id', 'DESC']], // Sort by id in descending order
     });
 
-    const destination = destinationData.get({ plain: true });
+    console.log('latestDestinations:', latestDestinations);
+
+
+    // Render the destination.handlebars template and pass in the data
     res.render('destination', {
-      ...destination,
-      loggedIn: req.session.logged_in,
+      ...destinationData.dataValues,
+      latestDestinations,
     });
-  } catch (err) {
-    console.log('err', err);
-    res.status(500).json(err);
+  } catch (error) {
+    console.error('Error fetching destination data:', error);
+    res.status(500).send('An error occurred while fetching destination data.');
   }
 });
+
+
 
 module.exports = router;
